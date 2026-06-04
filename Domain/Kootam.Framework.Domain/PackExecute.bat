7db602a3fccd98519a176d41f565b966efd0998b
Behnam Hadipanah <behnamhadipanah@outlook.com>
2026-06-04 10:25:26 +0200

    update delete ps1 file and bat

--- Domain/Kootam.Framework.Domain/PackExecute.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Running NuGet Pack Script
-echo Starting PowerShell script...
-
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\pack.ps1"
-
-echo.
-echo Process finished.
-pause
